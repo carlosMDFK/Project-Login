8a1d354b9004142d28d633c83a3407edaeff9ef1
carlosmdfk <karlos.dnklxz@gmail.com>
2022-06-03 19:15:44 -0400

    inicio

---
+++ index.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import ttk as ttk
+
+#ventana principal  a
+root = Tk()
+root.title("Login Usuario")
+
+#mainFrame, contenido  o
+mainFrame = Frame(root)
+mainFrame.pack()
+mainFrame.config(width=480, height=320)# bg="lightblue")
+
+#etiqueta
+titulo = Label (mainFrame, text="Login de Usuario con Python", font=("Arial",24))
+titulo.grid(column=0, row=0, padx=10, pady=10, columnspan=2)
+
+#Elementos de interfaz, textos y titulos
+nombreLabel = Label(mainFrame, text="Nombre: ")
+nombreLabel.grid(column=0, row=1)
+
+passLabel = Label(mainFrame, text="Contraseña: ")
+passLabel.grid(column=0, row=2)
+
+#Entradas de textos
+nombreUsuario = StringVar()
+nombreUsuario.set("Carlos")
+nombreEntry = Entry(mainFrame, textvariable=nombreUsuario)
+nombreEntry.grid(column=1, row=1)
+
+contraUsuario = StringVar()
+contraUsuario.set("1234")
+contraEntry = Entry(mainFrame, textvariable=contraUsuario, show="*")
+contraEntry.grid(column=1, row=2)
+
+#botones 
+iniciarSesionButton = ttk.Button(mainFrame, text="Iniciar sesion")
+iniciarSesionButton.grid(column=1, row=3, ipadx=5, ipady=5, padx=10, pady=10)
+
+registrarButton = ttk.Button(mainFrame, text="Registrar")
+registrarButton.grid(column=0, row=3, ipadx=5, ipady=5, padx=10, pady=10)
+
+root.mainloop()
---
+++ usuario.py
@@ -0,0 +1,59 @@
+from msilib import datasizemask
+
+class usuario():
+    
+    numUsuario = 0
+
+    def __init__(self, nombre, contra):
+        self.nombre = nombre
+        self.contra = contra
+
+        self.conectado = False
+        self.intentos = 3
+
+        usuario.numUsuario+=1
+
+    def conectar(self):
+        myContra = input ("Ingrese su contraseña: ")
+        if myContra==self.contra:
+            print("Se ha conectado con exito!")
+            self.conectado = True 
+#password mala  a
+        else:
+#se pierde 1 intento cada vez que se ingrese mal 
+            self.intentos-=1
+#si quedan intentos, mostrar mensajes y pedir dnvo credenciales
+            if self.intentos > 0:
+                print("Contraseña incorrecta, intentelo nuevamente...")
+                print("Intentos restantes:",self.intentos)
+                self.conectar()
+#si ya no tienes intentos, salir de app
+            else:
+                print("Error, no se pudo iniciar sesion...")
+                print("Adios")
+
+    def desconectar(self):
+        if self.conectado:
+            print("Se cerro sesion con exito!")
+            self.conectado = False
+        else:
+            print("Error, no se ha iniciado sesion")
+    
+    def __str__(self):
+        if self.conectado:
+            conect = "Conectado"
+        else:
+            conect = "Desconectado"
+        return f"Mi nombre de usuario es {self.nombre} y estoy {conect}"
+
+#despues de crear usuario, se muestra el estado
+user1 = usuario(input("Ingrese un nombre: "), input("Ingrese una contraseña: "))
+print(user1)
+
+#estado conectado
+user1.conectar()
+print(user1)
+
+#estado desconectado
+user1.desconectar()
+print(user1)
